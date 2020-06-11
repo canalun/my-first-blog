a09a820a33b857bb473796df5c08d3c4020899f9
canalun <kanakanarunrun@gmail.com>
2020-06-12 02:02:39 +0900

    created UMA report site top and post_list

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path("", views.post_list, name="post_list"),
-        path("post/<int:pk>", views.post_detail, name="post_detail")
+        path("", views.top, name="top"),
+        path("report", views.report, name="report"),
+        path("finds", views.post_list, name="finds"),
+        path("login", views.login, name="login"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,17 @@ from django.utils import timezone
 from .models import Post
 
 # Create your views here.
+def top(request):
+    return render(request, "blog/top.html", {})
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, "blog/post_list.html", {'posts':posts})
 
-def post_detail(request,pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, "blog/post_detail.html", {'post': post})
+def report(request):
+    # Create "Post form"
+    return render(request, "blog/report.html", {})
+
+def login(request):
+    # Create "login form"
+    return render(request, "blog/login.html", {})
